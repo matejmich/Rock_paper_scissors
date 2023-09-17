11adfa9547c6c27850c098fa51883a0602d9fcb5
matejmich <matejmich@seznam.cz>
2023-09-17 16:43:14 +0200

    first lines functios without the FUNCTION

---
+++ java.js
@@ -0,0 +1,7 @@
+getComputerChoice() {}
+var list;
+list = ["Rock", "Paper", "Scissors"];
+var computerChoice;
+computerChoice = list[Math.floor(Math.random()*list.length)]; 
+
+console.log(computerChoice)
