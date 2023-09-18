bc7a0bf1bf1750ab7eec872bbb1c6c5e6ab06317
matejmich <matejmich@seznam.cz>
2023-09-18 16:52:01 +0200

    Fully functional. All points of current lesson have been met.

--- java.js
+++ java.js
@@ -1,38 +1,55 @@
-function getComputerChoice(computerChoice) {
-    var list = ["ROCK", "PAPER", "SCISSORS"];
-    computerChoice = list[Math.floor(Math.random()*list.length)]; 
-    return computerChoice
-    
-    }
-function playRound(playerSelection, computerSelection) {
-    
-    playerSelection = "SCISSORS";
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = getComputerChoice();
-    if (computerSelection == playerSelection) {
-        return "Tie"
-    }
-    else if(computerSelection == "ROCK" && playerSelection == "PAPER") {
-        return "Congratulations! Paper beats Rock :)"
-    }
-    else if(computerSelection == "ROCK" && playerSelection == "SCISSORS") {
-        return "Sorry mate. Rock beats Scissors."
-    }
-    else if(computerSelection == "PAPER" && playerSelection == "SCISSORS") {
-        return "Congratulations! Scissors beat Paper :)"
-    }
-    else if(computerSelection == "PAPER" && playerSelection == "ROCK") {
-        return "Sorry mate, Paper beats Rock :("
-    }
-    else if(computerSelection == "SCISSORS" && playerSelection == "ROCK") {
-        return "Congratulations! Rock beats Paper ;)"
-    }
-    else if(computerSelection == "SCISSORS" && playerSelection == "PAPER") {
-        return "Sorry mate. Scissors beat Paper :/"
-    }
-    else {
-        return "Error? hádám"
-    }
-}
-console.log(playRound())
+var playerScore = 0
+var computerScore = 0
 
+    function getComputerChoice(computerChoice) {
+        var list = ["ROCK", "PAPER", "SCISSORS"];
+        computerChoice = list[Math.floor(Math.random()*list.length)]; 
+        return computerChoice
+
+        }
+    function playRound(playerSelection, computerSelection) {
+
+        playerSelection = prompt("Type rock, paper or scissors")
+        playerSelection = playerSelection.toUpperCase();
+        computerSelection = getComputerChoice();
+        if (computerSelection == playerSelection) {
+            return "Tie"
+        }
+        else if(computerSelection == "ROCK" && playerSelection == "PAPER") {
+            playerScore++
+            return "Congratulations! Paper beats Rock :)" 
+            
+        }
+        else if(computerSelection == "ROCK" && playerSelection == "SCISSORS") {
+            computerScore++
+            return "Sorry mate. Rock beats Scissors.";
+            
+        }
+        else if(computerSelection == "PAPER" && playerSelection == "SCISSORS") {
+            playerScore++
+            return "Congratulations! Scissors beat Paper :)";
+             
+        }
+        else if(computerSelection == "PAPER" && playerSelection == "ROCK") {
+            computerScore++
+            return "Sorry mate, Paper beats Rock :("
+            
+        }
+        else if(computerSelection == "SCISSORS" && playerSelection == "ROCK") {
+            playerScore++
+            return "Congratulations! Rock beats Paper ;)"
+            
+        }
+        else if(computerSelection == "SCISSORS" && playerSelection == "PAPER") {
+            computerScore++
+            return "Sorry mate. Scissors beat Paper :/" 
+        }
+        else {
+            return "You have put unknown value, reload and try again :)"
+        }
+    }
+
+for (;playerScore < 5 && computerScore < 5; console.log(playRound())) {
+    
+}
+console.log("You have: " + playerScore + "points and computer has: " + computerScore + "points")
