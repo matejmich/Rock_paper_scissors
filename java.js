02435b6b7c622e8ccda65367fb7f6123e0c95a8a
matejmich <matejmich@seznam.cz>
2023-09-18 15:12:58 +0200

    First part of the assigment works. The game displays how and whether you won or lost. Random choice works for computer as well

--- java.js
+++ java.js
@@ -1,8 +1,38 @@
-function getComputerChoice() {
-var list;
-list = ["Rock", "Paper", "Scissors"];
-var computerChoice;
-computerChoice = list[Math.floor(Math.random()*list.length)]; 
-return computerChoice + " was computer's choice!"
+function getComputerChoice(computerChoice) {
+    var list = ["ROCK", "PAPER", "SCISSORS"];
+    computerChoice = list[Math.floor(Math.random()*list.length)]; 
+    return computerChoice
+    
+    }
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = "SCISSORS";
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = getComputerChoice();
+    if (computerSelection == playerSelection) {
+        return "Tie"
+    }
+    else if(computerSelection == "ROCK" && playerSelection == "PAPER") {
+        return "Congratulations! Paper beats Rock :)"
+    }
+    else if(computerSelection == "ROCK" && playerSelection == "SCISSORS") {
+        return "Sorry mate. Rock beats Scissors."
+    }
+    else if(computerSelection == "PAPER" && playerSelection == "SCISSORS") {
+        return "Congratulations! Scissors beat Paper :)"
+    }
+    else if(computerSelection == "PAPER" && playerSelection == "ROCK") {
+        return "Sorry mate, Paper beats Rock :("
+    }
+    else if(computerSelection == "SCISSORS" && playerSelection == "ROCK") {
+        return "Congratulations! Rock beats Paper ;)"
+    }
+    else if(computerSelection == "SCISSORS" && playerSelection == "PAPER") {
+        return "Sorry mate. Scissors beat Paper :/"
+    }
+    else {
+        return "Error? hádám"
+    }
 }
-console.log(getComputerChoice())
+console.log(playRound())
+
